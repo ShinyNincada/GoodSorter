cb69c4b2a36f7953c3105fdfc091ef815d6117b6
ShinyNincada <106441989+ShinyNincada@users.noreply.github.com>
2023-09-22 12:03:40 +0700

    Sortable, but unable to complete remove old item

---
+++ Assets/Scripts/ToyObject.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToyObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public SlotHolder currentHolder;
+    [SerializeField] private ToyObjectSO toySO; 
+    [HideInInspector] Transform parentTransform;
+    public Image image;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentTransform = this.transform.parent;
+        currentHolder = parentTransform.GetComponentInParent<SlotHolder>();
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        // Set the z component to the distance between the object and the camera
+        mousePosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        
+        // Convert the screen coordinates to world coordinates
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        // Ignore the z-axis by keeping the original z value
+        worldPosition.z = transform.position.z;
+
+        // Update the object's position
+        transform.position = worldPosition;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentTransform);
+        currentHolder = parentTransform.GetComponentInParent<SlotHolder>();
+        image.raycastTarget = true;
+    }
+
+    public void SetParentTransform(Transform newParent){
+        parentTransform = newParent;
+    }
+
+    public ToyObjectSO GetToyObjectSO() {
+        return toySO;
+    }
+}
---
+++ Assets/Scripts/ToyObjectSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ToyObjectSO : ScriptableObject
+{
+   public string toyName;
+   public Sprite sprite;
+   public Transform prefabs;
+}
